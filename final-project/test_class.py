7fbe49b7b76fd5447780175f658b3a74f437542e
alfredwechselberger <alfred.wechselberger@gmail.com>
2020-05-01 10:21:29 -0400

    Playing with documention comments using RestructuredText format.

--- final-project/test_class.py
+++ final-project/test_class.py
@@ -1,12 +1,52 @@
 class test_class:
+    """
+    Testing out making a class
 
+    """
     def __init__(self):
         self.outStr = "hello world class"
 
-    def hello_world(self):
+
+    def hello_world_doc(first, second, third):
+        """
+        Some description text
+
+        :param second:
+        :type second:
+        :param third:
+        :type third:
+        :return: first + second
+        :rtype: float
+        :raises DimensionError: check dimension of matrices
+
+        """
+        return 1+1
+
+
+    def hello_world(arg1, arg2, arg3):
+        """
+        Testing hello word
+
+        :param arg1: the first value
+        :param arg2: the first value
+        :param arg3: the first value
+        :type arg1: int, float,...
+        :type arg2: int, float,...
+        :type arg3: int, float,...
+        :return: arg1/arg2 +arg3
+        .. todo:: check that arg2 is non zero.
+        """
+
         print(self.outStr)
+        return 1+1.5
 
     def hello_world2(self):
+        """
+        Testing hello word2
+
+        :returns: nothing
+        """
+
         print(self.outStr + " 2")
 
 
