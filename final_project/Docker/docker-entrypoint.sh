5ef1b06b205e10ba55ce34e9ee9e19b6b08a97a4
Alfred Wechselberger <alfred.wechselberger@gmail.com>
2020-05-29 00:09:12 -0400

    Setting up Docker environment

---
+++ final_project/Docker/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+
+
+#shopt -s extglob
+
+# testing
+# purple
+
+#echo 'root:cs230stanford' | chpasswd
+#sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+# SSH login fix. Otherwise user is kicked off after login
+#sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
+
+service ssh start
